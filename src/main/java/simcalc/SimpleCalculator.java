bf9e63866d0de2be88bb19cfb3a4c6489a4fe12e
kks <kkspp@naver.com>
2016-04-11 18:05:11 +0900

    정상이되어라

--- src/main/java/simcalc/SimpleCalculator.java
+++ src/main/java/simcalc/SimpleCalculator.java
@@ -13,14 +13,11 @@ public class SimpleCalculator {
 		result=x*y;
 	}
 	public void div(int x, int y) {
-<<<<<<< HEAD
 		if(y != 0)
-=======
 	//	if(y != 0)
 	//		result=x/y;
 	//	else
 			System.out.println("분모가 0입니다.");
->>>>>>> refs/heads/poolfix2
 	}
 	public int getResult(){
 		return result;
